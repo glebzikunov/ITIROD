55f78e58fe1cdc03183b7572b39cf3000a81df80
Gleb Zikunov <zikunovga@gmail.com>
2023-03-25 19:48:41 +0300

    feat: add firebase config file

---
+++ src/ServiceStation/js/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCDYUUMMOWbDufQYJ1lWEZHadrkvwzGkww",
+  authDomain: "servicestation-v1.firebaseapp.com",
+  projectId: "servicestation-v1",
+  storageBucket: "servicestation-v1.appspot.com",
+  messagingSenderId: "251267094450",
+  appId: "1:251267094450:web:e08c10e30f5b48119576ab",
+  measurementId: "G-3Q5W5H6GEJ"
+};
+
+// Initialize Firebase
+firebaseConfig.initializeApp(firebaseConfig);
