e6deed35e9ba20d5aed301fef97bef45ad9701d9
Gleb Zikunov <zikunovga@gmail.com>
2023-03-25 22:41:39 +0300

    feat: add user registration

---
+++ src/ServiceStation/js/firebase/signup.js
@@ -0,0 +1,53 @@
+const registerBtn = document.getElementById("signup").onclick = ((e) => {
+  e.preventDefault()
+
+  const email = document.getElementById("email").value
+  const password = document.getElementById("password").value
+
+  // Verify username and password
+  if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Email format is wrong!'
+    })
+    return false;
+  }
+
+  if (password.length < 8) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Password must contain more than 7 symbols!'
+    })
+    return false;
+  }
+
+  {
+    const registerDate = new Date();
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredentials) => {
+      firebase.firestore().collection("users").doc().set({
+        email: email,
+        userId: userCredentials.user.uid,
+        registrationDate: registerDate.getDate() + "." + (registerDate.getMonth() + 1) + "." + registerDate.getFullYear()
+      })
+      Swal.fire({
+        icon: 'success',
+        title: 'Good job!',
+        text: 'You have been successfully registered!',
+        confirmButtonText: 'Login'
+      }).then(function() {
+        window.location.href = "/html/login.html"
+      })
+    }).catch(error => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: `${error.message}`
+      })
+      return false;
+    })
+  }
+})
