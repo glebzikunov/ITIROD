c3830702bfba54c8ea1bf61e1734fb8127e0157b
Gleb Zikunov <zikunovga@gmail.com>
2023-03-26 16:23:37 +0300

    feat: add user logout

---
+++ src/ServiceStation/js/firebase/logout.js
@@ -0,0 +1,24 @@
+document.getElementById("logout").onclick = (() => {
+  firebase.auth().signOut().then(() => {
+    sessionStorage.removeItem("uid")
+    Swal.fire({
+      icon: 'success',
+      title: 'Good job!',
+      text: 'You have been successfully logged out!',
+      confirmButtonText: 'Login again'
+    }).then(function () {
+      window.location.href = "/html/login.html"
+      return false;
+    })
+  }).catch(error => {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: `${error.message}`,
+    }).then(function() {
+      return false;
+    })
+  })
+  
+  return false;
+})
