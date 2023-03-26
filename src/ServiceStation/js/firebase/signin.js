cb9c4a58f5a034e25b774f8ac39a2814704db19f
Gleb Zikunov <zikunovga@gmail.com>
2023-03-26 16:19:13 +0300

    feat: add user login

---
+++ src/ServiceStation/js/firebase/signin.js
@@ -0,0 +1,31 @@
+const loginBtn = document.getElementById("signin").onclick = ((e) => {
+  e.preventDefault()
+
+  const password = document.getElementById("password").value
+  const email = document.getElementById("email").value
+
+  // Verify username and password
+  if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Email format is wrong!'
+    })
+    return false;
+  } else {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredentials) => {
+      sessionStorage.setItem("uid", userCredentials.user.uid)
+      window.location.href = "/html/booking.html"
+    }).catch((error) => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: `${error.message}`,
+        confirmButtonText: 'Try again'
+      }).then(function() {
+        return false;
+      })
+    })
+  }
+}) 
